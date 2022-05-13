69d3c45101b046f81fecf9ad7e3f0e29bcddfd2b
Ellen <80380535+jcrescent61@users.noreply.github.com>
2022-05-13 19:22:52 +0900

    Project Setting

---
+++ DelegationExample/DelegationExample/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  DelegationExample
+//
+//  Created by Ellen on 2022/05/13.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# DelegationExam
+# DelegationExample
